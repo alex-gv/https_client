d0fddbdbfc8a07a48156d0afadc0fe9f97c3b892
Gurov Aleksei <agurov@evomarin.local>
2026-03-16 12:14:24 +0300

    Update system certs loader for IOS

--- src/apple/ssl_context_builder.cpp
+++ src/apple/ssl_context_builder.cpp
@@ -4,8 +4,10 @@
 #include <memory>
 #include <vector>
 #include <Security/Security.h>
-#include <Security/SecTrustSettings.h>
 #include <CoreFoundation/CoreFoundation.h>
+#ifdef ___OSX___
+#include <Security/SecTrustSettings.h>
+#endif
 
 namespace https_client {
 class SSLCustomContextBuilder::Impl {
@@ -23,13 +25,46 @@ class SSLCustomContextBuilder::Impl {
         if (!store) {
             throw std::runtime_error("Cannot get X509 store from SSL context");
         }
+#ifdef ___OSX___
         AddCertificatesFromKeychain(store, kSecTrustSettingsDomainUser);
         AddCertificatesFromKeychain(store, kSecTrustSettingsDomainAdmin);
         AddCertificatesFromKeychain(store, kSecTrustSettingsDomainSystem);
-
+#else
+        AddCertificatesFromKeychain(store);
+#endif
         boost::system::error_code ec;
         ctx.set_default_verify_paths(ec);
     }
+
+    void AddCertificatesFromKeychain(X509_STORE* store) {
+        CFArrayRef certs = nullptr;
+        OSStatus status = SecTrustCopyAnchorCertificates(&certs);
+        if (status != errSecSuccess || !certs) {
+            return;
+        }
+        CFIndex count = CFArrayGetCount(certs);
+        for (CFIndex i = 0; i < count; i++) {
+            SecCertificateRef sec_cert = (SecCertificateRef)CFArrayGetValueAtIndex(certs, i);
+
+            CFDataRef cert_data = SecCertificateCopyData(sec_cert);
+            if (!cert_data)
+                continue;
+
+            const UInt8* data = CFDataGetBytePtr(cert_data);
+            CFIndex length = CFDataGetLength(cert_data);
+
+            const unsigned char* p = data;
+            X509* x509 = d2i_X509(nullptr, &p, length);
+
+            if (x509) {
+                X509_STORE_add_cert(store, x509);
+                X509_free(x509);
+            }
+            CFRelease(cert_data);
+        }
+        CFRelease(certs);
+    }
+
     void AddCertificatesFromKeychain(X509_STORE* store, SecTrustSettingsDomain domain) {
         CFArrayRef certs = nullptr;
         OSStatus status = SecTrustSettingsCopyCertificates(domain, &certs);
